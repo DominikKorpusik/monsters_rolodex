63d134e42cfb88bb177e1680dfa91bf08abbfebf
Dominik Korpusik <dominik.korpusik@gmail.com>
2023-10-09 20:18:51 +0200

    Update Hooks

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,17 +1,27 @@
-import { Component } from "react";
+// import { Component } from "react";
 import "./search-box.styles.css";
 
-class SearchBox extends Component {
-  render() {
-    return (
-      <input
-        className={this.props.className}
-        type="search"
-        placeholder={this.props.placeholder}
-        onChange={this.props.onChangeHandler}
-      />
-    );
-  }
-}
+const SearchBox = ({ className, placeholder, onChangeHandler }) => (
+  <input
+    className={className}
+    type="search"
+    placeholder={placeholder}
+    onChange={onChangeHandler}
+  />
+);
+
+// class SearchBox extends Component {
+//   render() {
+//     const { className, placeholder, onChangeHandler } = this.props;
+//     return (
+//       <input
+//         className={className}
+//         type="search"
+//         placeholder={placeholder}
+//         onChange={onChangeHandler}
+//       />
+//     );
+//   }
+// }
 
 export default SearchBox;
